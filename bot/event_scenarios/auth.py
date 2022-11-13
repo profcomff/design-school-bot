2075efabbf530c18721626413c9af63ccb1026b4
MikeP <parfenov.ma21@physics.msu.ru>
2022-11-13 23:19:53 +0300

    registry admin spam

--- bot/event_scenarios/auth.py
+++ bot/event_scenarios/auth.py
@@ -3,14 +3,10 @@ import requests
 from bot.config import get_settings
 
 
-def _auth_headers():
-    settings = get_settings()
-    beaver = requests.post(
-        f"{settings.BACKEND_URL}/token",
-        {"username": settings.BACKEND_USER, "password": settings.BACKEND_PASSWORD},
-    )
-    auth_data = json.loads(beaver.content)
-    return {"Authorization": f"Bearer {auth_data.get('access_token')}"}
-
-
-auth_headers = _auth_headers()
+settings = get_settings()
+beaver = requests.post(
+     f"{settings.BACKEND_URL}/token",
+     {"username": settings.BACKEND_USER, "password": settings.BACKEND_PASSWORD},
+)
+auth_data = json.loads(beaver.content)
+auth_headers = {"Authorization": f"Bearer {auth_data.get('access_token')}"}
